6911ec22e2e02a105f363371821d70d28cb3f46b
ssori <115159126+ssori0421@users.noreply.github.com>
2023-07-03 22:26:01 +0900

    feat: 비동기 호출 일반화

--- src/api/getProductData.js
+++ src/api/getProductData.js
@@ -1,7 +1,22 @@
+// 에러 핸들링
+// 비동기 호출 일반화
+const request = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    const errData = await response.json();
+    throw errData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getProductData = async () => {
-  const response = await fetch('./api/productData.json');
-  const data = await response.json();
-  return data;
+  const result = await request('./api/productData.json');
+  return result;
 };
 
 export default getProductData;
