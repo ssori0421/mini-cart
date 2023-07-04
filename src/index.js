0d046f9deda0e3b132895a94273f57d95078a5f6
ssori <115159126+ssori0421@users.noreply.github.com>
2023-07-04 11:46:22 +0900

    feat: 상품 목록 렌더링

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import getProductData from './api/getProductData.js';
+import ProductList from './components/ProductList.js';
+
+const $productListGrid = document.getElementById('product-card-grid');
+
+const productList = new ProductList($productListGrid, []);
 
 const fetchProductData = async () => {
   const result = await getProductData();
-  console.log(result);
+  productList.setState(result);
 };
 
 fetchProductData();
