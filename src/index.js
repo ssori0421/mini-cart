038d91744f3bd5771c9fb6440c116fdc6ac9e6d0
ssori <115159126+ssori0421@users.noreply.github.com>
2023-07-04 12:17:23 +0900

    feat: 장바구니 토글 기능, backdrop

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import getProductData from './api/getProductData.js';
 import ProductList from './components/ProductList.js';
 
 const $productListGrid = document.getElementById('product-card-grid');
+const $openCartBtn = document.getElementById('open-cart-btn');
+const $closeCartBtn = document.getElementById('close-cart-btn');
+const $shoppingCart = document.getElementById('shopping-cart');
+const $backDrop = document.getElementById('backdrop');
 
 const productList = new ProductList($productListGrid, []);
+
+const toggleCart = () => {
+  $shoppingCart.classList.toggle('translate-x-full');
+  $shoppingCart.classList.toggle('translate-x-0');
+  $backDrop.hidden = !$backDrop.hidden; // hidden 속성으로 backdrop의 가시성 제어
+};
 
 const fetchProductData = async () => {
   const result = await getProductData();
@@ -11,3 +21,7 @@ const fetchProductData = async () => {
 };
 
 fetchProductData();
+
+$openCartBtn.addEventListener('click', toggleCart);
+$closeCartBtn.addEventListener('click', toggleCart);
+$backDrop.addEventListener('click', toggleCart);
